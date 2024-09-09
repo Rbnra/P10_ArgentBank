ab7a3a257c8fbbd83066f0fc9c232d4235484422
Rbnra <rimebnr@hotmail.com>
2024-09-09 22:59:22 +0200

    ajout du App

--- ArgentBank-Frontend-main/Argent-bank-react/src/App.tsx
+++ ArgentBank-Frontend-main/Argent-bank-react/src/App.tsx
@@ -3,21 +3,22 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Home } from './pages/Home/Home';
 import { SignIn } from './pages/SignIn/SignIn';
 import { Profil } from './pages/Profil/Profil';
-import './index.css';
+import { ProtectedRoute } from './components/ProtectedRoute/ProtectedRoute'; // Import de la route protégée
+import './index.scss';
 
-
-/** Composant principal de l'application. */
+/** Composant principal de l'application */
 export const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
-        <Route path="/profile" element={<Profil />} />
+
+        {/* Protégez la route du profil avec le composant ProtectedRoute */}
+        <Route element={<ProtectedRoute />}>
+          <Route path="/profile" element={<Profil />} />
+        </Route>
       </Routes>
     </Router>
   );
-}
-
-
-
+};
