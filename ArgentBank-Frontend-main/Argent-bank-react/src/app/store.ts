959f60b17ecea5d03adc0b857530573ef88c77ad
Rbnra <rimebnr@hotmail.com>
2024-09-11 20:07:03 +0200

    ajout du store.ts

--- ArgentBank-Frontend-main/Argent-bank-react/src/app/store.ts
+++ ArgentBank-Frontend-main/Argent-bank-react/src/app/store.ts
@@ -7,5 +7,12 @@ export const store = configureStore({
   },
 });
 
+//Pour rendre le store accessible dans la console du navigateur mais pas en mode production
+//utiliser store.getState() pour obtenir l'état actuel du store dans la console du navigateur
+if (process.env.NODE_ENV !== 'production') {
+    (window as any).store = store;
+}
+
+//Exportation du store
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
